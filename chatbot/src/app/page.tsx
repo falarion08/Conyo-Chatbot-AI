81cf69b72d15df905763f7755b349ad31d4aea59
falarion08 <barrion.ryanonil@gmail.com>
2024-08-12 12:19:10 +0800

    Added fonts

---
+++ chatbot/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { sf_pro } from "@/utils/fonts";
+
+
+
+export default function Home() {
+  return (
+    <>
+      <p className={` font-sfpro font-bold text-5xl`}>The Magical Planet</p>
+      <br></br>
+
+    </>
+    
+  );
+}
